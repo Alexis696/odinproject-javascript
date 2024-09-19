0b0fc4b52137dc4dce6b62e256693cb4be398c5e
Aleksis D. <alexinmercado68@gmail.com>
2024-09-20 00:49:07 +0200

    started creating functions for the board

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,3 +1,20 @@
-function createPlayer (name, symbol) {
+function createPlayer(name, symbol) {
+  
   return { name, symbol };
 }
+
+function gameBoard() {
+  let topL = " ";
+  let topM = " ";
+  let topR = " ";
+  let midL = " ";
+  let midM = " ";
+  let midR = " ";
+  let botL = " ";
+  let botM = " ";
+  let botR = " ";
+  
+}
+
+const gameLogic = (function () {
+})();
