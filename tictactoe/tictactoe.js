065c159c97b2bbc77e8f8cd561766c3e1240366c
Aleksis D. <alexinmercado68@gmail.com>
2024-09-25 23:04:50 +0200

    TUI done, starting on GUI now

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -5,7 +5,7 @@ function createPlayer(name, symbol) {
 }
 
 const GameBoard = (function() {
-  board = {
+  const board = {
     ["topL"]: " ",
     ["topM"]: " ",
     ["topR"]: " ",
@@ -26,13 +26,42 @@ ${board["botL"]} | ${board["botM"]} | ${board["botR"]}`);
   };
 
   const checkWinner = function(currentPlayer) {
-    if (currentPlayer.symbol === board["topL"]) {
+    if (
+      (currentPlayer.symbol === board["topL"] &&
+        currentPlayer.symbol === board["topM"] &&
+        currentPlayer.symbol === board["topR"]) ||
+      (currentPlayer.symbol === board["midL"] &&
+        currentPlayer.symbol === board["midM"] &&
+        currentPlayer.symbol === board["midR"]) ||
+      (currentPlayer.symbol === board["botL"] &&
+        currentPlayer.symbol === board["botM"] &&
+        currentPlayer.symbol === board["botR"]) ||
+      (currentPlayer.symbol === board["topL"] &&
+        currentPlayer.symbol === board["midL"] &&
+        currentPlayer.symbol === board["botL"]) ||
+      (currentPlayer.symbol === board["topM"] &&
+        currentPlayer.symbol === board["midM"] &&
+        currentPlayer.symbol === board["botM"]) ||
+      (currentPlayer.symbol === board["topR"] &&
+        currentPlayer.symbol === board["midR"] &&
+        currentPlayer.symbol === board["botR"]) ||
+      (currentPlayer.symbol === board["topL"] &&
+        currentPlayer.symbol === board["midM"] &&
+        currentPlayer.symbol === board["botR"]) ||
+      (currentPlayer.symbol === board["botL"] &&
+        currentPlayer.symbol === board["midM"] &&
+        currentPlayer.symbol === board["topR"])
+    ) {
       return true;
     }
     return false;
   };
 
-  return { board, printBoardTUI, checkWinner };
+  const checkTie = function() {
+    return Object.values(board).every(x => x !== ' ');
+  };
+
+  return { board, printBoardTUI, checkWinner, checkTie };
 })();
 
 function runTUI() {
@@ -64,9 +93,12 @@ function runTUI() {
     }
 
     console.log();
-    console.log(`It's ${currentPlayer.name}'s turn. Type where you want your mark: `);
+    console.log(
+      `It's ${currentPlayer.name}'s turn. Type where you want your mark: `,
+    );
     placement = prompt();
     GameBoard.board[placement] = currentPlayer.symbol;
+
     if (GameBoard.checkWinner(currentPlayer) === true) {
       console.clear();
       console.log("Final Board: ");
@@ -75,7 +107,20 @@ function runTUI() {
       console.log(`${currentPlayer.name} won! Do you want to play again?`);
       break;
     }
+
+    if(GameBoard.checkTie() === true) {
+      console.clear();
+      console.log("Final Board: ");
+      GameBoard.printBoardTUI();
+      console.log();
+      console.log(`It's a tie! Do you want to play again?`);
+      break;
+    }
   }
+}
+
+function runGUI() {
+  
 }
 
 runTUI();
