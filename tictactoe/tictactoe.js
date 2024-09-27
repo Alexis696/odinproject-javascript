d44dfb3aae5f7ae9f87b3376ce0994bfaeefa1c4
Aleksis D. <alexinmercado68@gmail.com>
2024-09-27 18:01:02 +0200

    started doing functions to manipulate display on tictactoe GUI

--- tictactoe/tictactoe.js
+++ tictactoe/tictactoe.js
@@ -1,7 +1,17 @@
+let listOfPlayers = [];
+
 const cells = document.querySelectorAll(".cell");
+const addPlayerButton = document.querySelector("#player-form");
 
 function createPlayer(name, symbol) {
   return { name, symbol };
+}
+
+function updatePlayerList() {
+  const playerTable = document.querySelector("#player-table");
+  playerTable.innerHTML = '';
+
+
 }
 
 function runGUI() {
@@ -10,6 +20,19 @@ function runGUI() {
       cells[i].innerText = "X";
     });
   }
+
+  addPlayerButton.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const playerName = document.getElementById("player-name").value;
+    const playerSymbol= document.getElementById("player-symbol").value;
+
+    const newPlayer = createPlayer(playerName, playerSymbol);
+    listOfPlayers.push(newPlayer);
+    console.log(listOfPlayers);
+
+    addPlayerButton.reset();
+  });
 }
 
 runGUI();
